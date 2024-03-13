c34b7b2f7a01611aeb1e6b33efb8139fae9832a6
charlesnyana <nyanasen@sheridancollege.ca>
2024-03-13 17:22:25 -0400

    task 4b completed: text outputs current selected villager. also removed debug code from thief. amended because forgot to save scene.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public GameObject textUI;
+    static TextMeshProUGUI typeText;
+    static string villagerType;
+
+    private void Start()
+    {
+        typeText = textUI.GetComponent<TextMeshProUGUI>();
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
+            typeText.text = "Select a Villager";
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        if (villager.ToString() == "Merchant (Merchant)") villagerType = "Merchant";
+        if (villager.ToString() == "Archer (Archer)") villagerType = "Archer";
+        if (villager.ToString() == "Thief (Thief)") villagerType = "Thief";
+        typeText.text = villagerType;
     }
     
 }
