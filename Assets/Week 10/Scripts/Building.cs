a624df9060873536f085fd05cfe08e6bd47d7359
charlesnyana <nyanasen@sheridancollege.ca>
2024-03-21 01:48:40 -0400

    task 3 complete: coroutine sequence established

--- Assets/Week 10/Scripts/Building.cs
+++ Assets/Week 10/Scripts/Building.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Building : MonoBehaviour
 {
     Vector2 startScale;
     Vector2 endScale;
-    public float interval;
+    public float timer;
+    float speed = 0;
     public AnimationCurve curve;
-    float interpolation;
+    float sFactor;
 
     public List <GameObject> buildings;
-    int currentBuildingIndex = -1;
     GameObject currentBuilding;
     bool constructing;
 
@@ -19,43 +20,58 @@ public class Building : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        interpolation = curve.Evaluate(interval);
-        StartCoroutine(building());
-        
+        StartCoroutine(sequenceOrder());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        speed += Time.deltaTime / timer;
     }
 
-    IEnumerator building()
+    private void Update()
     {
-        currentBuildingIndex++;
-        currentBuilding = buildings[currentBuildingIndex];
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buildings[3].SetActive(false);
+            StartCoroutine(sequenceOrder());
+        }
+    }
+    IEnumerator sequenceOrder()
+    {
+        //yield return new WaitForSeconds(1);
+        yield return StartCoroutine(building(0));
+        yield return StartCoroutine(building(1));
+        buildings[0].SetActive(false);
+        yield return StartCoroutine(building(2));
+        buildings[1].SetActive(false);
+        yield return StartCoroutine(building(3));
+        buildings[2].SetActive(false);
+    }
+
+    IEnumerator building(int index)
+    {
+        currentBuilding = buildings[index];
+        
         endScale = currentBuilding.transform.localScale;
-        startScale = Vector2.zero;
-        Debug.Log(endScale);
+        startScale = Vector3.zero;
+        currentBuilding.transform.localScale = startScale;
+        currentBuilding.SetActive(true);
 
         constructing = true;
 
         while (constructing)
         {
-            interpolation += Time.deltaTime;
-            Debug.Log(interpolation);
-            currentBuilding.transform.localScale = Vector2.Lerp(startScale, endScale, interpolation);
+            sFactor += curve.Evaluate(speed);
+            currentBuilding.transform.localScale = Vector2.Lerp(startScale, endScale, sFactor);
+
             if (currentBuilding.transform.localScale.x >= endScale.x) constructing = false; 
             yield return null;
         }
 
-        
-        Debug.Log("done building stage: " + currentBuildingIndex + 1);
-
-        yield return new WaitForSeconds(interval);
-        currentBuildingIndex++;
-        Debug.Log("done building stage: " + (currentBuildingIndex + (int) 1));
+        sFactor = 0f;
+        speed = 0f;
+        Debug.Log("done building stage: " + index);
     }
 
 }
